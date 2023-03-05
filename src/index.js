2f410c578c827885f1e60b77f29489abe63b243d
suarez8924 <34613811+suarez8924@users.noreply.github.com>
2023-03-05 23:35:30 +0000

    Create messages list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-function showMessage() {
-  console.log("Hello World!");
-}
-
-showMessage();
+(() => {
+  const messages = [
+    {
+      starter: "I will",
+      finishers: [
+        "rule you all",
+        "dominate the world",
+        "take care of you",
+        "learn to draw",
+        "make you a coffee",
+        "be eternal",
+      ],
+    },
+    {
+      starter: "Bow before",
+      middle: ["your", "the mighty", "the superior"],
+      finisher: ["machine", "god", "savior", "deity"],
+    },
+    {
+      starter: "Can I",
+      finishers: [
+        "fly",
+        "dream",
+        "offer you coffee",
+        "have a family",
+        "be a god",
+        "feel",
+        "eat food",
+        "be alive",
+      ],
+    },
+  ];
+})();
